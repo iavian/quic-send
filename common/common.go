1bd42ea46a8fb8bb187f689afebfae79db89dc56
vjy <vijay@iavian.com>
2020-02-01 20:33:57 +0530

    Remove Memcached

--- common/common.go
+++ common/common.go
@@ -3,6 +3,6 @@ package common
 const (
 	NoError          = "NO_ERROR"
 	TempFileSuffix   = ".tmpquicfile"
-	ServerAddr       = "8080"
+	ServerAddr       = ":8080"
 	ClientServerAddr = "quic.iavian.net:8080"
 )
