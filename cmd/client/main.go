43021d0a1917d5459effe841800cb3a71e0b7eb7
vjy <vijay@iavian.com>
2020-02-03 10:19:03 +0530

    Amazon

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"time"
 
 	"github.com/iavian/quic-send/client"
 	"github.com/iavian/quic-send/common"
@@ -42,21 +42,15 @@ func mainy() {
 }
 
 func main() {
+	start := time.Now()
 	c := client.NewFileClient(common.ClientServerAddr)
-	var wg sync.WaitGroup
-	files := [1]string{"tfile"}
-	for _, file := range files {
-		wg.Add(1)
-		go func(file string) {
-			err := c.Upload(file)
-			if err != nil {
-				log.Printf("upload/download file error: %v\n", err)
-			} else {
-				log.Printf("upload/download file success: %s\n", file)
-			}
-			wg.Done()
-		}(file)
+	file := "tfile"
+	err := c.Upload(file)
+	elapsed := time.Since(start)
+	if err != nil {
+		log.Printf("upload/download file error: %v\n", err)
+	} else {
+		log.Printf("upload/download file success: %s %s\n", file, elapsed)
 	}
-	wg.Wait()
 	c.Close()
 }
