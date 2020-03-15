8ae5a9c1fdce7eeb6cd63fb34af8655c35cd612c
vjy <vijay@iavian.com>
2020-03-15 18:01:29 +0530

    Added version

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,24 +1,75 @@
 package main
 
 import (
+	"bufio"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/iavian/quic-send/client"
 	"github.com/iavian/quic-send/common"
+	"github.com/lucas-clemente/quic-go"
+	"github.com/lucas-clemente/quic-go/http3"
 )
 
 func main() {
-	start := time.Now()
-	c := client.NewFileClient(common.ClientServerAddr)
-	defer c.Close()
-	file := "tfile"
-	err := c.Upload(file)
-	elapsed := time.Since(start)
-	if err != nil {
-		log.Printf("upload/download file error: %v\n", err)
+	if len(os.Args) > 1 {
+		start := time.Now()
+		c := client.NewFileClient(common.ClientServerAddr)
+		defer c.Close()
+		file := "tfile"
+		err := c.Upload(file)
+		elapsed := time.Since(start)
+		if err != nil {
+			log.Printf("upload/download file error: %v\n", err)
+		} else {
+			log.Printf("upload/download file success: %s %s\n", file, elapsed)
+		}
 	} else {
-		log.Printf("upload/download file success: %s %s\n", file, elapsed)
+		pool, err := x509.SystemCertPool()
+		if err != nil {
+			log.Fatal(err)
+		}
+		roundTripper := &http3.RoundTripper{
+			TLSClientConfig: &tls.Config{
+				RootCAs:            pool,
+				InsecureSkipVerify: true,
+			}, DisableCompression: false, QuicConfig: &quic.Config{KeepAlive: true},
+		}
+		roundTripper.QuicConfig.GetLogWriter = func(connectionID []byte) io.WriteCloser {
+			filename := fmt.Sprintf("client_%x.qlog", connectionID)
+			f, err := os.Create(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Creating qlog file %s.\n", filename)
+			return struct {
+				io.Writer
+				io.Closer
+			}{bufio.NewWriter(f), f}
+		}
+		defer roundTripper.Close()
+		hclient := &http.Client{
+			Transport: roundTripper,
+		}
+		start := time.Now()
+		file, err := os.Open("tfile")
+		if err != nil {
+			panic(err)
+		}
+		res, err := hclient.Post("https://quic.iavian.net:8080/upload", "binary/octet-stream", file)
+		elapsed := time.Since(start)
+		if err != nil {
+			panic(err)
+		} else {
+			log.Printf("upload/download file success: %s %s\n", file.Name(), elapsed)
+		}
+		defer res.Body.Close()
+		fmt.Println("All done")
 	}
-
 }
